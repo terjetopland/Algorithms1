1b2cfa84823d076ebfd55b249c17901238306c6a
Terje Topland <terjetop@uia.no>
2022-08-18 13:48:42 +0200

    multiplic. algorithm

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    // Multiplication algorithm.
+    int x, y;
+    cout << "Please enter two numbers and their output will be x * y" << endl;
+    cin >> x >> y;
+    int multiplication = x*y;
+    cout << "x * y = " << multiplication;
+
+
+    return 0;
+}
