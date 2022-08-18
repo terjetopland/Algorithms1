e75aec5e852ba51fc7526d373cb20765db5b3973
Terje Topland <terjetop@uia.no>
2022-08-18 16:27:18 +0200

    Sum of all numbers between n and 1

--- main.cpp
+++ main.cpp
@@ -2,14 +2,35 @@
 #include <cstdlib>
 
 using namespace std;
+int multi2Numbers(const string& someOutputToUser) {
+    cout << someOutputToUser << endl;
+    int x, y;
+    cin >> x >> y;
+    int multiplication = x * y;
+
+    return multiplication;
+}
+
+int sumAllNumbersFromNTillOne(int n) {
+
+    if (n > 1) {
+        return n + sumAllNumbersFromNTillOne(n - 1);
+    } else {
+        return 1;
+    }
+}
+
 
 int main() {
     // Multiplication algorithm.
-    int x, y;
-    cout << "Please enter two numbers and their output will be x * y" << endl;
-    cin >> x >> y;
-    int multiplication = x*y;
-    cout << "x * y = " << multiplication;
+
+    int result = multi2Numbers("Please enter two numbers to multiply!");
+    cout << "Result = " << result << endl;
+
+    int number;
+    cout << "Please enter a valid integer." << endl;
+    cin >> number;
+    cout <<  "sum of numbers between " << number << " and 1 = " << sumAllNumbersFromNTillOne(number) << endl;
 
 
     return 0;
